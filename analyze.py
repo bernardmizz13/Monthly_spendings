f01cb6eda3de5693f2b0602fe8f11de28d3d53f3
Bernard Mizzi <bernard.mizzi@gmail.com>
2024-08-09 11:53:59 +0200

    Read also the monthly earnings for the year

--- analyze.py
+++ analyze.py
@@ -5,11 +5,20 @@ import sys # for command line arguments
 
 
 
-# read the csv, pass the year as a command line argument when running the script
-df = pd.read_csv("data/" + sys.argv[1] + "_monthly_spendings.csv", sep = ",")
+# read the monthly spendings csv, pass the year as a command line argument when running the script
+df_spendings = pd.read_csv("data/" + sys.argv[1] + "_monthly_spendings.csv", sep = ",")
 
-print("\nSuccessfully read the CSV for year", sys.argv[1])
+print("\nSuccessfully read the monthly spendings CSV for year", sys.argv[1])
 
 print("\nCSV file info:")
 
-print(df.info)
+print(df_spendings.info)
+
+# read the monthly earnings csv, pass the year as a command line argument when running the script
+df_earnings = pd.read_csv("data/" + sys.argv[1] + "_monthly_earnings.csv", sep = ",")
+
+print("\nSuccessfully read the monthly earnings CSV for year", sys.argv[1])
+
+print("\nCSV file info:")
+
+print(df_earnings.info)
