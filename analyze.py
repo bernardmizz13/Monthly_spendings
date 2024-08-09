f31ae000bbf9d1981bd79dff82eeeae1654e7a86
Bernard Mizzi <bernard.mizzi@gmail.com>
2024-08-09 13:46:42 +0200

    Group by month and then event

--- analyze.py
+++ analyze.py
@@ -3,8 +3,6 @@ import numpy as np # for mathematical use
 import matplotlib.pyplot as plt # for data visualisation
 import sys # for command line arguments
 
-
-
 # read the monthly spendings csv, pass the year as a command line argument when running the script
 df_spendings = pd.read_csv("data/" + sys.argv[1] + "_monthly_spendings.csv", sep = ",")
 
@@ -22,3 +20,23 @@ print("\nSuccessfully read the monthly earnings CSV for year", sys.argv[1])
 print("\nCSV file info:")
 
 print(df_earnings.info)
+
+# retrieve the months
+months = df_spendings.Month.unique()
+
+print("\n", months)
+
+# initialize a new dataframe to store monthly stats in it
+df = pd.new_("")
+
+# iterate the data using the month
+for m in months:
+    df_month = df_spendings[df_spendings["Month"] == m]
+    print("Retrieved the data for", m)
+    # retrieve the event type
+    events = df_month.Event.unique()
+    # iterate the data using the event
+    for e in events:
+        df_month_event = df_month[df_month["Event"] == e]
+        
+    print(df)
