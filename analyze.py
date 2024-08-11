45f0d18e99046418a2470f6a9275b3df50b2b97a
Bernard Mizzi <bernard.mizzi@gmail.com>
2024-08-11 15:22:18 +0200

    Plot yearly event spending

--- analyze.py
+++ analyze.py
@@ -4,6 +4,19 @@ import matplotlib.pyplot as plt # for data visualisation
 import sys # for command line arguments
 import os # to create directory
 
+if not os.path.exists('stats'):
+    os.mkdir('stats')
+    
+if not os.path.exists('plots'):
+    os.mkdir('plots')
+
+# make the directory for the year if it does not exist
+if not os.path.exists('stats/' + sys.argv[1]):
+    os.mkdir('stats/' + sys.argv[1])
+
+if not os.path.exists('plots/' + sys.argv[1]):
+    os.mkdir('plots/' + sys.argv[1])
+
 # read the monthly spendings csv, pass the year as a command line argument when running the script
 df_spendings = pd.read_csv("data/" + sys.argv[1] + "_monthly_spendings.csv", sep = ",")
 
@@ -53,17 +66,31 @@ for m in months:
         
     print("\nCompleted stats for", m)
 
-if not os.path.exists('stats/' + sys.argv[1]):
-    os.mkdir('stats/' + sys.argv[1])
+if not os.path.exists('stats/' + sys.argv[1] + '/month'):
+    os.mkdir('stats/' + sys.argv[1] + '/month')
 
-stats.to_csv("stats/" + sys.argv[1] + "/" + sys.argv[1] + "_monthly_stats.csv", index = False)
+stats.to_csv("stats/" + sys.argv[1] + "/month/" + sys.argv[1] + "_monthly_stats.csv", index = False)
 
 ###########################################################
+
+if not os.path.exists('stats/' + sys.argv[1] + '/events'):
+    os.mkdir('stats/' + sys.argv[1] + '/events')
+    
+if not os.path.exists('plots/' + sys.argv[1] + '/events'):
+    os.mkdir('plots/' + sys.argv[1] + '/events')
+    
+if not os.path.exists('plots/' + sys.argv[1] + '/year'):
+    os.mkdir('plots/' + sys.argv[1] + '/year')
+    
+if not os.path.exists('stats/' + sys.argv[1] + '/year'):
+    os.mkdir('stats/' + sys.argv[1] + '/year')
 
 print("----------------------------")
 print("\nWill now plot line graphs!")
 
 events = stats.Event.unique()
+
+e_yearly = pd.DataFrame(columns = ["Event", "Amount", "Monthly_average"])
 
 # check each event and then each month to see what was spent on that event each month
 for e in events:
@@ -83,11 +110,12 @@ for e in events:
             amounts = amounts + [df_event.Amount.tolist()[0]]
 
     # create a dataframe
-    e_monthly = pd.DataFrame({"Month": months, "Amount": amounts, "Average_per_month": [np.round(np.average(amounts), 2)] * len(months)})
-    e_monthly.to_csv("stats/" + sys.argv[1] + "/" + sys.argv[1] + "_" + e + "_monthly_spendings.csv")
-
-    plt.figure()
+    e_monthly = pd.DataFrame({"Month": months, "Amount": amounts})
+    e_monthly.to_csv("stats/" + sys.argv[1] + "/events/" + sys.argv[1] + "_" + e + "_monthly_spendings.csv", index = False)
     
+    e_yearly = e_yearly.append({"Event": e, "Amount": np.round(np.sum(amounts), 2), "Monthly_average": np.round(np.average(amounts), 2)}, ignore_index = True)
+            
+    plt.figure()
 
     # Plot the first line
     plt.plot(months, amounts, marker='o', linestyle='-', color='b', label='Amount spent EUR ' + str(np.round(np.sum(amounts), 2)))
@@ -103,17 +131,59 @@ for e in events:
     plt.legend()
 
     # Display the graph
-    plt.savefig("plots/" + sys.argv[1] + "/" + sys.argv[1] + "_" + e + "_monthly_spendings.png")        
+    plt.savefig("plots/" + sys.argv[1] + "/events/" + sys.argv[1] + "_" + e + "_monthly_spendings.png")        
     plt.close()
     # plot for each month
     print("\nPlotted amounts spent monthly for " + e)
+    print("Total: " + str(np.sum(amounts)))
+    print("Monthly average: " + str(np.round(np.average(amounts), 2)))
+
+# Define the positions of the bars
+bar_width = 0.35
+index = np.arange(len(e_yearly.Event.tolist()))
+
+# Increase the width of the figure
+plt.figure(figsize=(20, 10))  # Width = 10, Height = 6
+# Create the bar chart with two sets of bars
+plt.bar(e_yearly.Event.tolist(), e_yearly.Amount.tolist())
+    
+# Add a title and labels
+plt.title('Yearly event spendings')
+plt.xlabel('Event')
+plt.ylabel('Spending in EUR')
+plt.grid(True)
+plt.xticks(index + bar_width / 2, e_yearly.Event.tolist())
+# Add a legend
+#plt.legend()
+plt.savefig("plots/" + sys.argv[1] + "/year/" + sys.argv[1] + "_event_spendings.png")
+plt.close()
+
+plt.figure(figsize=(20, 10))  # Width = 10, Height = 6
+
+# Create the bar chart with two sets of bars
+plt.bar(e_yearly.Event.tolist(), e_yearly.Monthly_average.tolist())
+    
+# Add a title and labels
+plt.title('Monthly average event spendings')
+plt.xlabel('Event')
+plt.ylabel('Monthly average spending in EUR')
+plt.grid(True)
+plt.xticks(index + bar_width / 2, e_yearly.Event.tolist())
+# Add a legend
+#plt.legend()
+plt.savefig("plots/" + sys.argv[1] + "/year/" + sys.argv[1] + "_monthly_average_event_spendings.png")
+plt.close()
+
+e_yearly.to_csv("stats/" + sys.argv[1] + "/year/" + sys.argv[1] + "_yearly_spendings.csv", index = False)
+
+print("\nSaved and plotted the yearly stats")
     
 ###########################################################
 print("----------------------------")
 print("\nWill now plot pie charts!")
 
-if not os.path.exists('plots/' + sys.argv[1]):
-    os.mkdir('plots/' + sys.argv[1])
+if not os.path.exists('plots/' + sys.argv[1] + '/month'):
+    os.mkdir('plots/' + sys.argv[1] + '/month')
 
 # iterate the stats using the month
 for m in months:
@@ -132,13 +202,20 @@ for m in months:
     # Add a title
     plt.title("Monthly spendings for " + m + " " + sys.argv[1], fontsize=25)
     # Save the pie chart
-    plt.savefig("plots/" + sys.argv[1] + "/" + sys.argv[1] + "_" + m + "_spendings.png")
+    plt.savefig("plots/" + sys.argv[1] + "/month/" + sys.argv[1] + "_" + m + "_spendings.png")
     plt.close()
     print("\nPlotted for", m)
  
 ###########################################################
 
 if sys.argv[2] is 'y':
+
+    if not os.path.exists('plots/' + sys.argv[1] + '/sports'):
+        os.mkdir('plots/' + sys.argv[1] + '/sports')
+        
+    if not os.path.exists('stats/' + sys.argv[1] + '/sports'):
+        os.mkdir('stats/' + sys.argv[1] + '/sports')
+    
     print("----------------------------")
     print("\nWill now count how much money was spent on 5aside football and how much we attended in " + sys.argv[1])
     
@@ -173,8 +250,8 @@ if sys.argv[2] is 'y':
     # append the data in the stats csv
     total_sports = total_sports.append({"Attended": att, "Amount": total, "Average_att_per_month": np.round(att/len(months), 2), "Average_spent_per_month": np.round(total/len(months), 2)}, ignore_index = True)
     
-    total_sports.to_csv("stats/" + sys.argv[1] + "/" + sys.argv[1] + "_sports_yearly_stats.csv", index = False)
-    monthly_sports.to_csv("stats/" + sys.argv[1] + "/" + sys.argv[1] + "_sports_monthly_stats.csv", index = False)
+    total_sports.to_csv("stats/" + sys.argv[1] + "/sports/" + sys.argv[1] + "_sports_yearly_stats.csv", index = False)
+    monthly_sports.to_csv("stats/" + sys.argv[1] + "/sports/" + sys.argv[1] + "_sports_monthly_stats.csv", index = False)
 
     print("\nStats saved!")
     
@@ -199,7 +276,7 @@ if sys.argv[2] is 'y':
     plt.legend()
 
     # Display the graph
-    plt.savefig("plots/" + sys.argv[1] + "/" + sys.argv[1] + "_sports_stats.png")
+    plt.savefig("plots/" + sys.argv[1] + "/sports/" + sys.argv[1] + "_sports_stats.png")
     plt.close()
     
     print("\nPlotted sports data!")
